b0073963cdd6fd2c05dbb3fc25f927ec7a979233
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-09-23 09:31:52 -0300

    feature: criando conexao com banco de dados e injecao de dependencias e usando migrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,46 +13,26 @@ namespace ProEventos.API.Controllers
 	[Route("api/[controller]")]
 	public class EventoController : ControllerBase
 	{
+		private readonly DataContext _context;
 
-		public EventoController()
+		public EventoController(DataContext context)
 		{
+			_context = context;
 		}
 
-		public IEnumerable<Evento> _evento = new Evento[]
-			{
-				new Evento()
-				{
-					EventoId = 1,
-					Tema = "Angular e .Net 5",
-					Local = "BH",
-					Lote = "1 lote",
-					QtdPessoas = 20,
-					DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-					ImagemURL = "https://github.com/gabrieljuniorferrari88.png",
-				},
-				new Evento()
-				{
-					EventoId = 2,
-					Tema = "dotNet MVC",
-					Local = "BH",
-					Lote = "1 lote",
-					QtdPessoas = 10,
-					DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-					ImagemURL = "https://github.com/gabrieljuniorferrari88.png",
-				},
-
-			};
 
 		[HttpGet]
 		public IEnumerable<Evento> Get()
 		{
-			return _evento;
+			return _context.Eventos;
 		}
 
 		[HttpGet("{id}")]
-		public IEnumerable<Evento> GetById(int id)
+		public Evento GetById(int id)
 		{
-			return _evento.Where(x => x.EventoId == id);
+			return _context.Eventos.FirstOrDefault(
+				evento => evento.EventoId == id
+			);
 		}
 
 		[HttpPost]
